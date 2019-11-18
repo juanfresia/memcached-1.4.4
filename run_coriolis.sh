7db068e70a94f3f9e654f38a2c9d36ac60df7d63
Juan Manuel Fresia <juanmanuelfresia@gmail.com>
2019-11-18 16:37:38 +0000

    run_coriolis.sh
    Signed-off-by: Juan Manuel Fresia <juanmanuelfresia@gmail.com>

---
+++ run_coriolis.sh
@@ -0,0 +1,10 @@
+apt-get install -y libevent-dev
+cd memcached-1.4.4 && ./configure && make && make install && cd ..
+cd libmemcached-0.49 && ./configure && make && make install && cd ..
+ldconfig
+
+memcached -u root -t 2 & 
+
+cd reproduce-pkg && make && ./trigger
+
+sleep 10
